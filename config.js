ad451163ac9f8a2a4a393096c4edfe36eb20afa3
GuiJiAg <guillermojjagudo@gmail.com>
2019-05-09 21:50:11 +0200

    Commit

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     port: process.env.Port || 3000,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/hacebuche',
+    db: process.env.MONGODB || 'mongodb+srv://GuiJiAg:hacemongo@hacebuche-atxn7.mongodb.net/hacebuche?retryWrites=true',
+    //db: process.env.MONGODB || 'mongodb://localhost:27017/hacebuche',
     SECRET_TOKEN: 'hacemongo'
 };
