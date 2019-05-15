955910c4744e36eff4af54796fe9ff18cb51514f
GuiJiAg <guillermojjagudo@gmail.com>
2019-05-15 11:12:19 +0200

    Commit

--- controllers/mail.js
+++ controllers/mail.js
@@ -5,9 +5,6 @@ const nodemailer = require('nodemailer');
 const config = require('../config');
 
 function sendEmail(req, res) {
-    console.log('POST /api/wines/albarinios');
-    console.log(req.body);
-
     let mail = new Mail();
     mail.name = req.body.name;
     mail.email = req.body.email;
@@ -18,7 +15,7 @@ function sendEmail(req, res) {
     mail.preferedPlace = req.body.preferedPlace;
     mail.observations = req.body.observations;
 
-    let transporter = nodemailer.createTransport({
+    let transporterClient = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
           type: config.authGmail,
@@ -52,11 +49,48 @@ function sendEmail(req, res) {
         '<p>¡Esperamos verle pronto!</p>'
     }
 
-    transporter.sendMail(mailOptions, (err, info) => {
-    if (err) throw new Error(err)
+    transporterClient.sendMail(mailOptions, (err, info) => {
+        if (err) throw new Error(err)
 
-    res.statusCode = 200;
-    res.end('Email sent!');
+        res.statusCode = 200;
+        res.end('Email sent!');
+    })
+
+    let transporterOwner = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          type: config.authGmail,
+          user: config.email,
+          clientId: config.clientId,
+          clientSecret: config.clientSecret,
+          refreshToken: config.refreshToken
+        }
+    })
+    
+    mailOptions = {
+        from: config.email,
+        to: config.email,
+        subject: 'Solicitud de reserva',
+        html: 
+        '<h1>Se ha registrado una nueva solicitud de reserva</h1>'+
+        '<h3>Estos son los datos de la solicitud:</h3>'+
+        `<p>Nombre: <strong>${mail.name}</strong></p>`+
+        `<p>Email: <strong>${mail.email}</strong></p>`+
+        `<p>Teléfono: <strong>${mail.phone}</strong></p>`+
+        `<p>Fecha de la reserva: <strong>${mail.reserveDate}</strong></p>`+
+        `<p>Hora de la reserva: <strong>${mail.reserveHour}</strong></p>`+
+        `<p>Número de comensales: <strong>${mail.numberDiners}</strong></p>`+
+        `<p>Zona preferente: <strong>${mail.preferedPlace}</strong></p>`+
+        `<p>Observaciones: <strong>${mail.observations}</strong></p>`+
+        '<br /><br /><hr />'+
+        '<p>Confirme o rechace la reserva contactando de forma directa con el cliente</p>'
+    }
+
+    transporterOwner.sendMail(mailOptions, (err, info) => {
+        if (err) throw new Error(err)
+
+        res.statusCode = 200;
+        res.end('Email sent!');
     })
 }
 
