31013089e4c0e15f6d80ff4970b81427950a0e48
GuiJiAg <guillermojjagudo@gmail.com>
2019-05-21 20:06:51 +0200

    Preparing to Heroku

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     port: process.env.Port || 3000,
-    //db: process.env.MONGODB || 'mongodb+srv://GuiJiAg:hacemongo@hacebuche-atxn7.mongodb.net/hacebuche?retryWrites=true',
-    db: process.env.MONGODB || 'mongodb://localhost:27017/hacebuche',
+    db: process.env.MONGODB || 'mongodb+srv://GuiJiAg:hacemongo@hacebuche-atxn7.mongodb.net/hacebuche?retryWrites=true',
+    //db: process.env.MONGODB || 'mongodb://localhost:27017/hacebuche',
     SECRET_TOKEN: 'hacemongo',
     authGmail: 'OAuth2',
     email: 'guillermojjagudo@gmail.com',
